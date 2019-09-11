f394df29609f785b998d314c63a3ed1758f3c39a
Jay Fallon <jayfallon@gmail.com>
2019-09-11 09:09:35 -0400

    lol fixed test

--- test/api.test.js
+++ test/api.test.js
@@ -1,26 +1,26 @@
-const request = require('supertest');
-const express = require('express');
-const api = require('../api/hello');
+const request = require("supertest");
+const express = require("express");
+const api = require("../api/hello");
 
 const app = express();
-app.get('/api/hello', api);
+app.get("/api/hello", api);
 
-beforeAll((done) => {
+beforeAll(done => {
   server = app.listen(process.env.PORT || 3000, () => {
     console.log(`Server running on port ${process.env.PORT || 3000}.`);
-    done()
-  });;
+    done();
+  });
 });
 
-describe('GET /api/hello', function(){
-  it('respond with "Hello World!"', function(done){
+describe("GET /api/hello", function() {
+  it('respond with "Hello Jay!"', function(done) {
     request(app)
-      .get('/api/hello')
+      .get("/api/hello")
       .expect(200)
-      .expect("Hello World!", done)
-  })
+      .expect("Hello World!", done);
+  });
 });
 
-afterAll((done) => {
+afterAll(done => {
   server.close(done);
 });
